3028e167f65edba6b094cc3f75f5593cdeab912e
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-01-31 07:15:32 +0900

    admin_create

--- application/views/urls.py
+++ application/views/urls.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # app routing to vue-router
-urlpatterns += [re_path('.*', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path('.*', login_required(TemplateView.as_view(template_name='index.html')))]

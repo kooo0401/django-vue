eafe70223ea4c764529e87bb0d656aef0705ebbb
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-02-03 02:14:45 +0900

    store_create

---
+++ application/vuejs/src/store/header.js
@@ -0,0 +1,18 @@
+import KanbanClient from '../../utils.kanbanClient';
+
+
+const state = {
+};
+
+const actions = {
+};
+
+const mutations = {
+};
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+};

656637ae266b1cddc3a451a644b24ae9d7b5f76d
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-02-02 11:03:25 +0900

    kanbanClient_js_create

---
+++ application/vuejs/utils/kanbanClient.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import { loadProgressBar } from 'axios-progress-bar';
+import camelcaseKeys from 'camelcase-keys';
+
+
+const _handleSuccess = response => {
+  console.log(response.data);
+  // 全てのJSONのキーをスネークケースからキャメルケースに変換する //
+  response.data = camelcaseKeys(response.data, { deep: true});
+  return response;
+};
+
+class Client {
+
+  constructor() {
+    this.service = axios.create();
+    this.service.interceptors.response.use(_handleSuccess);
+    loadProgressBar({ showSpinner: false }, this.service);
+  }
+
+  _get(path,payload) {
+    return this.service.get(path, payload);
+  }
+
+  _patch(path, payload) {
+    return this.service.patch(path, payload);
+  }
+
+  _post(path, payload, config = {}) {
+    return this.service.post(path, payload, config);
+  }
+
+  _put(path, payload) {
+    return this.service.put(path, payload);
+  }
+
+  _delete(path) {
+    return this.service.delete(path);
+  }
+
+}
+
+class KanbanClient extends Client {
+
+  constructor() {
+    super();
+    this.baseUrl = '/api';
+  }
+
+  async getAccountInfo() {
+  const response = await this._get(`${this.baseUrl}/accounts/`);
+  return response.data.accountInfo;
+  }
+
+}
+
+export default new KanbanClient();

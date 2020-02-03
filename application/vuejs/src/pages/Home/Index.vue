006cd73bd733abdd7ff95dec499b18eba5369053
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-02-03 16:26:13 +0900

    index.vue_apply

--- application/vuejs/src/pages/Home/Index.vue
+++ application/vuejs/src/pages/Home/Index.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="home">
     <div class="row no-gutters">
-
+      {{ boardList }}
     </div>
   </div>
 </template>
 
 <script>
+
+import { createNamespacedHelpers } from 'vuex';
+const { mapState, mapActions } = createNamespacedHelpers('home');
+
 export default {
   name: 'home',
+  computed: {
+    ...mapState([
+      'boardList',
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'fetchBoardList',
+    ]),
+  },
+  async created() {
+    this.fetchBoardList();
+  },
 };
 </script>
 <style lang="scss" scoped>

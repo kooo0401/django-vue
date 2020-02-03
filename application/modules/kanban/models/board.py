f9c0728b8d9b7005d05d07a8ee851c15704c9dbd
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-02-03 15:35:07 +0900

    board_model_create

---
+++ application/modules/kanban/models/board.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+
+class Board(models.Model):
+    # 所有者
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # ボード名
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}: {} of {}'.format(self.pk, self.name, self.owner)

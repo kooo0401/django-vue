b20f236b9a434ce99b9ded348ea64ab1c81d7aa0
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-02-03 17:57:21 +0900

    service.py_create

--- application/modules/kanban/service.py
+++ application/modules/kanban/service.py
@@ -3,3 +3,15 @@ from .models import Board
 
 def get_board_list_by_owner(owner):
     return Board.get_list_by_owner(owner=owner)
+
+def add_board(owner, board_name):
+    """
+    :param User owner:
+    :param str board_name:
+    :return:
+    """
+    board = Board.objects.create(
+        owner=owner,
+        name=board_name,
+    )
+    return board
